9cd95a5102ab9b75d092e5be2f0cd1c8f0c0b93b
nealgod <gasalneal09123@gmail.com>
2025-09-22 03:58:56 +0800

    Added new seeders and updated code

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create admin user if it doesn't exist
+        $admin = User::firstOrCreate(
+            ['email' => 'ojt360admin@gmail.com'],
+            [
+                'name' => 'OJT360 Admin',
+                'password' => Hash::make('12345678'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ]
+        );
+
+        if ($admin->wasRecentlyCreated) {
+            $this->command->info('Admin user created successfully!');
+        } else {
+            $this->command->info('Admin user already exists!');
+        }
+        
+        $this->command->info('Email: ojt360admin@gmail.com');
+        $this->command->info('Password: 12345678');
+    }
+}
