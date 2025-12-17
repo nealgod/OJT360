fbaa85299fb6387d38b474c88feb82a410b8d6ff
nealgod <gasalneal09123@gmail.com>
2025-12-18 00:01:28 +0800

    Remove demo companies from seeder

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -12,28 +12,7 @@ class CompanySeeder extends Seeder
     {
         // Map some programs/departments by name for demo; companies table currently stores department as string
         $deptIt = Department::where('name', 'Department of Computer Studies')->first();
-        $deptEdu = Department::where('name', 'Department of Teacher Education')->first();
-        $deptEng = Department::where('name', 'Department of Engineering')->first();
 
-        
-
-        Company::firstOrCreate(['name' => 'EduCare Center'], [
-            'department' => $deptEdu?->name ?? 'Department of Teacher Education',
-            'address' => '45 Learning St, City',
-            'contact_person' => 'Mr. Santos',
-            'contact_email' => 'contact@educare.test',
-            'contact_phone' => '0917-000-2222',
-            'status' => 'active',
-        ]);
-
-        Company::firstOrCreate(['name' => 'BuildRight Engineering'], [
-            'department' => $deptEng?->name ?? 'Department of Engineering',
-            'address' => '789 Structure Rd, City',
-            'contact_person' => 'Engr. Cruz',
-            'contact_email' => 'apply@buildright.test',
-            'contact_phone' => '0917-000-3333',
-            'status' => 'active',
-        ]);
 
         // BSIT Companies - Real Data
         Company::firstOrCreate(['name' => 'Mac Builders'], [
