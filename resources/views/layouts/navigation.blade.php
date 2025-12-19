5e9177e8c613bfd547be3c445694906fcde3c1ef
nealgod <gasalneal09123@gmail.com>
2025-12-19 11:38:46 +0800

    fixed coordinator responsive not showing some navigations

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -284,6 +284,7 @@
                     </svg>
                     {{ __('Weekly Reports') }}
                 </x-responsive-nav-link>
+
                 <x-responsive-nav-link :href="route('messages.index')" :active="request()->routeIs('messages.*')">
                     <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
